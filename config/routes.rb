3d4b4a903e7d2ab2253ff68df1d6665038519e19
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-02 15:51:03 +0000

    Modify route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get("/", controller: "user", action: "home")
 
   get("/users", controller: "user", action: "user_list")
-  get("/users/:user_id", controller: "user", action: "user_profile")
+  get("/users/:username", controller: "user", action: "user_profile")
 
   get("/photos", controller: "photo", action: "photo_list")
   get("/photos/:photo_id", controller: "photo", action: "photo_info")
