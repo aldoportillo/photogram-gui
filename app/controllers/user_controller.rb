d8c057189f455c9f9f96653ea62181090aae6957
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-02 16:13:07 +0000

    Make user shows page dynamic

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,22 @@
 class UserController < ApplicationController
 
   def home
+    @users = User.order(:username)
     render({:template => "user/index"})
   end
 
   def user_list
+    @users = User.order(:username)
     render({:template => "user/index" })
   end
 
   def user_profile
+    username = params.fetch(:username)
+    
+    @user = User.where({:username => username}).at(0)
+
+    @user_photos = @user.own_photos
+
     render({:template => "user/show"})
   end
 end
