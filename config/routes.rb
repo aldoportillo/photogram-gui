acde1c49239df9992bc68cf05b057755e7e4860c
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-02 14:51:49 +0000

    Create routes for all get pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
 
+  get("/", controller: "", action: "")
+  
+  get("/users", controller: "", action: "")
+  get("/users/:user_id", controller: "", action: "")
+
+  get("/photos", controller: "", action: "")
+  get("/photos/:photo_id", controller: "", action: "")
+
 end
