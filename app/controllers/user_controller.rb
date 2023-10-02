69851f972eb6f5baa043208b41376968795b1198
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-02 16:59:03 +0000

    Create actions for new post routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -19,4 +19,35 @@ class UserController < ApplicationController
 
     render({:template => "user/show"})
   end
+
+  def add_user
+    @username = params.fetch("input_username")
+
+    new_user = User.new
+
+    new_user.username = @username
+
+    if new_user.valid?
+      new_user.save
+      redirect_to("/users/#{@username}", { :notice => "User created successfully." })
+    else
+      redirect_to("/users", { :notice => "User failed to create successfully." })
+    end
+  end
+  def update_user
+    @user_id = params.fetch("user_id")
+    @new_username = params.fetch("input_username")
+
+    user = User.where(:id => @user_id).at(0)
+
+    user.username = @new_username
+
+    if user.valid?
+      user.save
+      redirect_to("/users/#{@new_username}", { :notice => "User username updated successfully." })
+    else
+      redirect_to("/users", { :notice => "User failed to update username." })
+    end
+  end
+
 end
