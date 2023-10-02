d6a1f603a296b8ba7a5591af8cfe0332ade14f94
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-02 17:02:43 +0000

    Create routes for photo post actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
 
   get("/users", controller: "user", action: "user_list")
   get("/users/:username", controller: "user", action: "user_profile")
-  
+  post("/add_user", controller: "user", action: "add_user")
+  post("/update_user/:user_id", controller: "user", action: "update_user")
+
 
   get("/photos", controller: "photo", action: "photo_list")
   get("/photos/:photo_id", controller: "photo", action: "photo_info")
-
-  post("/add_user", controller: "user", action: "add_user")
-  post("/update_user/:user_id", controller: "user", action: "update_user")
+  post("/insert_photo_record", controller: "photo", action: "new_photo")
+  post("/update_photo/:photo_id", controller: "photo", action: "update_photo")
+  post("/delete_photo/:photo_id", controller: "photo", action: "delete_photo")
+  
 end
