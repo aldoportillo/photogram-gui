088cac45f1dcf74303ddb7034cbb8cabe3e9d911
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-02 16:54:04 +0000

    Create user post routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   get("/users", controller: "user", action: "user_list")
   get("/users/:username", controller: "user", action: "user_profile")
+  
 
   get("/photos", controller: "photo", action: "photo_list")
   get("/photos/:photo_id", controller: "photo", action: "photo_info")
 
+  post("/add_user", controller: "user", action: "add_user")
 end
